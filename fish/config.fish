22ea320035e13eb1591c9d4fd226f6745c76e194
sininen-blue <alaanneil@gmail.com>
2023-12-10 00:20:57 +0800

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+	alias ls='lsd'
+	alias l='ls -l'
+	alias la='ls -a'
+	alias lla='ls -la'
+	alias lt='ls --tree'
+
+    fish_add_path ~/bin
+    fish_add_path ~/go/bin
+end
